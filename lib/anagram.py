e9ef6adf677937a2cf99e0dfc345623bf017d470
Akuei <akueidok911@gmail.com>
2023-07-16 22:16:03 -0400

    Lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, name):
+        self.name = name
+
+    def match(self,value):
+        new_list = []
+        for elem in value:
+            if sorted(list(self.name))==sorted(list(elem)):
+                new_list.append(elem)
+        return new_list
